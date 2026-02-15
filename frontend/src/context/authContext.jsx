3a22b3ebe299ca6be1478439a6d398e2b3ba5d3e
yatirajpowar <yatirajpowar@gmail.com>
2026-02-16 02:59:51 +0530

    chnaged some lines to resolve case sensative issue on aws

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { authAPI } from "../services/api";
+import { authAPI } from "../Services/api";
 
 const AuthContext = createContext(null);
 
